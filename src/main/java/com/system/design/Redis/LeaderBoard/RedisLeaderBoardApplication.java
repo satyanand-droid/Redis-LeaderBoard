38c41176cc0be01b9daa2d8d9104e083e38495e6
satyanand deva <62107720+satyanand-droid@users.noreply.github.com>
2024-09-20 22:57:03 +0530

    Initial commit

---
+++ src/main/java/com/system/design/Redis/LeaderBoard/RedisLeaderBoardApplication.java
@@ -0,0 +1,13 @@
+package com.system.design.Redis.LeaderBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLeaderBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLeaderBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/design/Redis/LeaderBoard/RedisLeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.design.Redis.LeaderBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLeaderBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
